9aec858f4f195d404689d69875d615100f56ddc8
Maycon Oliveira <mayconbrito1998@hotmail.com>
2025-02-22 19:31:15 -0300

    up

--- src/util/uuidV4.ts
+++ src/util/uuidV4.ts
@@ -2,7 +2,9 @@
 const regexUuidV4 = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
 /* NUNCA ALTERAR ESSA REGEX. ELA ESTA SENDO UTILIZADA NA HORA DE CRIAR AS TABELAS COM ID UUID */
 
-export const uuidV4Test = (value: string | undefined) => {
+const uuidV4 = (value: string | undefined) => {
   if (!value) return false;
   return regexUuidV4.test(value);
 };
+
+export { uuidV4 };
